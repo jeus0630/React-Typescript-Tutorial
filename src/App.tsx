e2a4c18567b4edae1f5b1c44112005227771184e
jewoo <jeus0630@designfever.com>
2022-02-08 23:07:32 +0900

    Docs : Button, input
    Event Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import './App.css';
-import Status from "./components/Status";
-import Heading from "./components/Heading";
-import Oscar from "./components/Oscar";
-import Greet from "./components/Greet";
+import Button from "./components/Button";
+import Input from "./components/Input";
 
 function App() {
 
     return (
         <div className="App">
-            <Status status={"loading"}></Status>
-            <Heading>Placeholder text</Heading>
-            <Oscar>
-                <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
-            </Oscar>
-            <Greet name={"Jewoo"} isLoggedIn={true}></Greet>
+            <Button handleClick={(event,id)=>{
+                console.log('Button clicked', event, id);
+            }}></Button>
+            <Input value={''} handleChange={event => {
+                console.log(event.target.value);
+            }}></Input>
         </div>
     );
 }
