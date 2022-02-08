88a326392d866649ec03085f1fa1f8dd9969ac5f
jewoo <jeus0630@designfever.com>
2022-02-08 23:28:13 +0900

    Refactoring : Types
    Types Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import Container from "./components/Container";
 
 function App() {
 
     return (
         <div className="App">
-            <Container styles={{border: '1px solid black', padding: '1rem'}}></Container>
+
         </div>
     );
 }
