8b9fdac97a09fd51c68ebb923e935d8c30f25a01
jewoo <jeus0630@designfever.com>
2022-02-08 23:13:55 +0900

    Docs : Container
    Style Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import './App.css';
-import Button from "./components/Button";
-import Input from "./components/Input";
+import Container from "./components/Container";
 
 function App() {
 
     return (
         <div className="App">
-            <Button handleClick={(event,id)=>{
-                console.log('Button clicked', event, id);
-            }}></Button>
-            <Input value={''} handleChange={event => {
-                console.log(event.target.value);
-            }}></Input>
+            <Container styles={{border: '1px solid black', padding: '1rem'}}></Container>
         </div>
     );
 }
