6aa578c1fa2f32ac1605fd57b509c5f32f8e6123
jewoo <jeus0630@designfever.com>
2022-02-08 19:03:18 +0900

    Docs : Person, PersonList
    Basic Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import './App.css';
 import Greet from "./components/Greet";
+import Person from "./components/Person";
+import PersonList from "./components/PersonList";
 
 function App() {
+    const personName = {
+        first: 'Bruce',
+        last: 'Wayne'
+    }
+
+    const nameList = [
+        {
+            first: 'Bruce',
+            last: 'Wayne'
+        },
+        {
+            first: 'Clark',
+            last: 'Kent'
+        },
+        {
+            first: 'Princess',
+            last: 'Diana'
+        }
+    ]
+
     return (
         <div className="App">
-            <Greet name={"jewoo"}></Greet>
+            <Greet name={"jewoo"} messageCount={10} isLoggedIn={false}></Greet>
+            <Person name={personName}></Person>
+            <PersonList names={nameList}></PersonList>
         </div>
     );
 }
