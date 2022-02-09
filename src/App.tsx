337c7d96c3def940700476df2b5fdd97730fe482
jewoo <jeus0630@designfever.com>
2022-02-09 13:08:11 +0900

    Docs : List
    Generic Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import './App.css';
-import Private from "./components/auth/Private";
-import Profile from "./components/auth/Profile";
-
+import List from "./components/generics/List";
 function App() {
 
     return (
         <div className="App">
-            <Private isLoggedIn={true} Component={Profile}></Private>
+            {/*<List items={['Batman','Superman','Wonder woman']} onClick={(item)=>console.log(item)}></List>*/}
+            {/*<List items={[1,2,3]} onClick={(item)=>console.log(item)}></List> */}
+            <List items={[
+                {
+                    id: 1,
+                    first: 'Bruce',
+                    last: 'Wayne'
+                },
+                {
+                    id: 2,
+                    first: 'Clark',
+                    last: 'Kent'
+                },
+                {
+                    id: 3,
+                    first: 'Princess',
+                    last: 'Diana'
+                }
+            ]} onClick={(item)=>console.log(item)}></List>
         </div>
     );
 }
